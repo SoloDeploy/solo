59ae90ea5fa7aadf988852db5ce9a92253e1c774
Craig Anderson <1877372+CDA0@users.noreply.github.com>
2021-06-14 13:53:54 +0100

    test

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -3,7 +3,7 @@ package solo
 import "testing"
 
 func TestNewCmdSolo(t *testing.T) {
-	cmd := NewCmdSolo()
+	cmd := NewCmdSolo("0.0.0", "commitid", "date")
 
 	if cmd.Use != "solo" {
 		t.Errorf("Use is not correct")
