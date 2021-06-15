06d27fee09f57a3dc5ff59702c51e95773fae804
Craig Anderson <1877372+CDA0@users.noreply.github.com>
2021-06-15 08:58:57 +0100

    version

--- cmd/version/main.go
+++ cmd/version/main.go
@@ -1,13 +1,13 @@
 package version
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	goVersion "go.hein.dev/go-version"
+
+	"github.com/SoloDeploy/solo/core/output"
 )
 
-var output = "json"
+var outputFmt = "json"
 
 // NewCmdVersion creates a command to output the current version of SoloDeploy
 func NewCmdVersion(version string, commit string, date string) *cobra.Command {
@@ -16,12 +16,12 @@ func NewCmdVersion(version string, commit string, date string) *cobra.Command {
 		Short: "Version will output the current build information",
 		Long:  "Prints the version, Git commit ID and commit date in JSON or YAML format using the go.hein.dev/go-version package.",
 		Run: func(_ *cobra.Command, _ []string) {
-			resp := goVersion.FuncWithOutput(false, version, commit, date, output)
-			fmt.Print(resp)
+			resp := goVersion.FuncWithOutput(false, version, commit, date, outputFmt)
+			output.PrintfInfo(resp)
 		},
 	}
 
-	c.Flags().StringVarP(&output, "output", "o", "json", "Output format. One of 'yaml' or 'json'.")
+	c.Flags().StringVarP(&outputFmt, "output", "o", "json", "Output format. One of 'yaml' or 'json'.")
 
 	return c
 }
